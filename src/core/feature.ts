7a9235fe8536a7e164ae8c024e2eaa36808d720f
HyScript7 <hyscript7@seznam.cz>
2026-04-18 18:27:44 +0200

    feat: Implement a Feature builder

--- src/core/feature.ts
+++ src/core/feature.ts
@@ -1,4 +1,5 @@
 import Module from "./module";
+import type EventListener from "./listener";
 
 /**
  * A feature is a class responsible for initializing a Module.
@@ -41,5 +42,104 @@ export default abstract class Feature {
 
     public getDescription(): string {
         return this.description;
+    }
+}
+
+/**
+ * A builder for simple features.
+ * 
+ * Example usage:
+ * ```
+ * // index.ts of the feature
+ * export default new FeatureBuilder()
+ *     .withName("myFeature")
+ *     .withDescription("My feature description")
+ *     .addListener(() => new MyListener())
+ *     .build();
+ * ```
+ * 
+ * ## On the topic of instantiating services
+ * 
+ * If you need to instantiate services in the constructor or more advanced setup (for example, sharing a single service instance between multiple listeners), you have two options:
+ *   1. Extend the Feature class manually and initialize the service in the constructor.
+ *   Example:
+ * ```ts
+ * class MyFeature extends Feature {
+ *   constructor() {
+ *     super("myFeature", "My feature description");
+ * 
+ *     const someService = new SomeService();
+ * 
+ *     this.module.addCommand(new MyCommand(someService));
+ *     this.module.addListener(new MyListener(someService));
+ *   }
+ * }
+ * ```
+ * In the example above, `someService` will be initialized in the constructor of `MyFeature`. As such, every instance of this feature will have it's own instance of `someService`.
+ * 
+ *   2. If the service can be initialized as a singleton (i.e., it can be safely shared between multiple instances of this feature), you can provide it as an argument to the listeners and commands without extending the class.
+ *   Example:
+ * ```ts
+ * const someService = new SomeService(); // Will be shared between all created instances of the feature!
+ * const Feature = new FeatureBuilder()
+ *   .withName("myFeature")
+ *   .withDescription("My feature description")
+ *   .addListener(() => new MyListener(someService))
+ *   .addCommand(() => new MyCommand(someService))
+ *   .build();
+ * const featureOne = new Feature();
+ * const featureTwo = new Feature();
+ * ```
+ * In the example above, `someService` will be shared between both `featureOne` and `featureTwo`.
+ */
+export class FeatureBuilder {
+    private name: string | null;
+    private description: string | null;
+    private listenerProviders: (() => EventListener<any[]>)[];
+    private commandProviders: (() => any)[];
+
+    constructor() {
+        this.name = null;
+        this.description = null;
+        this.listenerProviders = [];
+        this.commandProviders = [];
+    }
+
+    public withName(name: string): FeatureBuilder {
+        this.name = name;
+        return this;
+    }
+
+    public withDescription(description: string): FeatureBuilder {
+        this.description = description;
+        return this;
+    }
+
+    public addListener(listenerProvider: () => EventListener<any[]>): FeatureBuilder {
+        this.listenerProviders.push(listenerProvider);
+        return this;
+    }
+
+    public addCommand(commandProvider: () => any): FeatureBuilder {
+        this.commandProviders.push(commandProvider);
+        return this;
+    }
+
+    public build(): new () => Feature {
+        if (this.name === null || this.description === null) {
+            throw new Error("Name and description must be set before building the feature");
+        }
+        const name = this.name;
+        const description = this.description;
+        const listenerProviders = this.listenerProviders;
+        const commandProviders = this.commandProviders;
+        return class extends Feature {
+            constructor() {
+                super(name, description);
+                const module = this.getModule();
+                listenerProviders.forEach(provider => module.addListener(provider()));
+                commandProviders.forEach(provider => module.addCommand(provider()));
+            }
+        }
     }
 }
--- src/modules/example/index.ts
+++ src/modules/example/index.ts
@@ -1,13 +1,16 @@
-import Feature from "../../core/feature";
+import { FeatureBuilder } from "../../core/feature";
 import SimpleListener from "./listeners/ready.listener";
 import AdvancedListener from "./listeners/advanced.listener";
 
-export default class ExampleFeature extends Feature {
-    constructor() {
-        super("example", "Example feature");
-        this.getModule().addListener(new SimpleListener());
-        this.getModule().addListener(new AdvancedListener({
-            doSomething: () => "Hello world"
-        }));
-    }
-}
+export default new FeatureBuilder()
+  .withName("example")
+  .withDescription("Example feature")
+  .addListener(() => new SimpleListener())
+  .addListener(
+    () =>
+      // Instantiation of a listener with the injection of an inline service exposing a doSomething method
+      new AdvancedListener({
+        doSomething: () => "Hello world",
+      }),
+  )
+  .build();
