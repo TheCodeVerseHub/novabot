5a45d4a5aa48750378f7beae376ee8bf0093e97e
HyScript7 <hyscript7@seznam.cz>
2026-04-18 18:13:49 +0200

    feat: Load token from .env and load modules

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Client, ClientEvents, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const token = process.env.BOT_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+import Loader from "./core/loader";
+
+const loader = new Loader();
+
+for (const feature of loader.discoverFeatures()) {
+  const instance = new feature();
+  for (const eventListener of instance.getModule().getListeners()) {
+    client.on(eventListener.getEvent() as keyof ClientEvents, async (...args) => {
+      await eventListener.listener(...args);
+    });
+  }
+}
+
+client.login(token);
