8c050127311ad860628c20bb24316dd2df610eac
HyScript7 <hyscript7@seznam.cz>
2026-04-18 17:00:46 +0200

    Define module command and listener container

---
+++ src/core/module.ts
@@ -0,0 +1,43 @@
+import type EventListener from "./listener.ts"
+
+/**
+ * A container for instances of commands and event listeners.
+ * 
+ * Example usage:
+ * ```ts
+ * const module = new Module();
+ * module.addCommand(new MyCommand());
+ * module.addListener(new MyListener());
+ * ```
+ * 
+ * It should be initialized by a Feature.
+ * @see {@link Feature}
+ */
+export default class Module {
+    // TODO: Command system integration
+    private readonly commands: any[]
+    private readonly listeners: EventListener<any[]>[]
+
+    constructor() {
+        this.commands = [];
+        this.listeners = [];
+    }
+
+    // TODO: Command system integration
+    public getCommands(): any[] {
+        return this.commands;
+    }
+
+    public getListeners(): EventListener<any[]>[] {
+        return this.listeners;
+    }
+
+    // TODO: Command system integration
+    public addCommand(command: any): void {
+        this.commands.push(command);
+    }
+
+    public addListener(listener: EventListener<any[]>): void {
+        this.listeners.push(listener);
+    }
+}
